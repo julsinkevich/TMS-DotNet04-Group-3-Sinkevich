08920c7916a9f30c2dc5f1a91af4d3fa0a1c74f8
Julia Sinkevich <66641254+julsinkevich@users.noreply.github.com>
2020-11-07 19:48:46 +0300

    fix: add gitignore

---
+++ src/TMS.ShopCheckoutSimulator/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMS.ShopCheckoutSimulator.Models
+{
+    class Product
+    {
+        public string Code { get; set; } = Guid.NewGuid().ToString();
+
+        public override string ToString()
+        {
+            return Code;
+        }
+    }
+}
---
+++ src/TMS.ShopCheckoutSimulator/Models/Terminal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMS.ShopCheckoutSimulator.Models
+{
+    class Terminal
+    {
+        private Basket Basket { get; set; }
+    }
+}
